8979e7a70af092a85bd0678820dc52343d4e12c5
Castracani <willmyers993@gmail.com>
2018-06-23 11:02:37 -0400

    ???

--- assets/javascript/time.js
+++ assets/javascript/time.js
@@ -21,42 +21,61 @@
       var date = $("#date-input").val().trim();
       var rate = $("#rate-input").val().trim();
 
-      var newRow = $("<tr>");
-      for (i = 0; i < 6; i++) {
-         var newColumn = $("<td>");
-         newColumn.text(name);
-         newColumn.text(role);
-         newColumn.append(date);
-         newColumn.append(rate);
-         $("#")
-      };
+      //var newRow = $("<tr>");
+      //for (i = 0; i < 6; i++) {
+      //   var newColumn = $("<td>");
+      //   newColumn.text(name);
+      //   newColumn.text(role);
+      //   newColumn.append(date);
+      //   newColumn.append(rate);
+      //   $("#")
+      //};
 
       database.ref().push({
           name: name,
           role: role,
           date: date,
           rate: rate,
-      })
+      });
 
 
   });
 
   dataRef.ref().on("child_added", function(childSnap) {
-      console.log(childSnap.val().name)
-      console.log(childSnap.val().role)
-      console.log(childSnap.val().date)
-      console.log(childSnap.val().rate)
-      console.log(childSnap.val().name)
-      console.log(childSnap.val().name)
+      console.log(childSnap.val().name);
+      console.log(childSnap.val().role);
+      console.log(childSnap.val().date);
+      console.log(childSnap.val().rate);
+      console.log(childSnap.val().name);
+      console.log(childSnap.val().name);
 
       var employeeTable = $("#employeeTable");
       var newRow = $("<tr>");
-      var nameCell = $("<td>")
-      var roleCell = $("<td>")
-      var startDateCell = $("<td>")
-      var rateCell = $("<td>")
-      var monthlyRateCell = $("<td>")
-      var totalBilledCell = $("<td>")
+      var nameCell = $("<td>");
+      var roleCell = $("<td>");
+      var startDateCell = $("<td>");
+      var rateCell = $("<td>");
+      var monthsWorkedCell = $("<td>");
+      var totalBilledCell = $("<td>");
+
+      nameCell.text(childSnap.val().name);
+      roleCell.text(childSnap.val().role);
+      startDateCell.text(childSnap.val().date);
+      rateCell.text(childSnap.val().rate);
+      monthsWorkedCell.text();
+      totalbilledCell.text();
+
+      console.log(nameCell);
+
+      newRow.append(nameCell);
+      newRow.append(roleCell);
+      newRow.append(startDateCell);
+      newRow.append(rateCell);
+      newRow.append(monthsWorkedCell);
+      newRow.append(totalBilledCell);
+
+
+
 
       $("#full-member-list").append("<div class='employee-table'><span class='employee-name'> " + childSnap.val().name + 
       " </span><span class='employee-role'> " + childSnap.val().role +
@@ -64,8 +83,8 @@
       " </span><span class='employee-rate'> " + childSnap.val().rate + "</span></div>");
 
       //error-handlling
-   }, function(errorObject) {
-     $("");
+   //}, function(errorObject) {
+     //$("");
       
 
 
