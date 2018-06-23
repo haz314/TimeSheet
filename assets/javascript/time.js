21dba1f8aa7393fef1fb385c92f559d790298093
Castracani <willmyers993@gmail.com>
2018-06-23 10:22:54 -0400

    lmao what

--- assets/javascript/time.js
+++ assets/javascript/time.js
@@ -21,6 +21,16 @@
       var date = $("#date-input").val().trim();
       var rate = $("#rate-input").val().trim();
 
+      var newRow = $("<tr>");
+      for (i = 0; i < 6; i++) {
+         var newColumn = $("<td>");
+         newColumn.text(name);
+         newColumn.append(role);
+         newColumn.append(date);
+         newColumn.append(rate);
+         $("#")
+      };
+
       database.ref().push({
           name: name,
           role: role,
