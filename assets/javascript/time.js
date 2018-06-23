1981a3b57106b09cf4c71ac75f85254ec317697b
Castracani <willmyers993@gmail.com>
2018-06-23 10:05:06 -0400

    lmao what am i doing

---
+++ assets/javascript/time.js
@@ -0,0 +1,32 @@
+
+//<script src="https://www.gstatic.com/firebasejs/5.1.0/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA80D5vpl2RVHgMMHE71ElHN1cPgXtSzXI",
+    authDomain: "employee-database-e9904.firebaseapp.com",
+    databaseURL: "https://employee-database-e9904.firebaseio.com",
+    projectId: "employee-database-e9904",
+    storageBucket: "",
+    messagingSenderId: "765794147328"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  //on-click event to generate new rows and tables
+  $("#submit-btn").on("click", function() {
+      var name = $("#name-input").val().trim();
+      var role = $("#role-input").val().trim();
+      var date = $("#date-input").val().trim();
+      var rate = $("#rate-input").val().trim();
+
+      database.ref().push({
+          name: name,
+          role: role,
+          date: date,
+          rate: rate,
+      })
+
+
+  });
